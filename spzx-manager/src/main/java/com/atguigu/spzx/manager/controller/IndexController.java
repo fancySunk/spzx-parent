899f14bf8c403e4872d129c06a5c92dbea6f8d53
SunKai <975147954@qq.com>
2024-05-25 17:52:38 +0800

    feat(controller):获取用户信息

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.atguigu.spzx.manager.controller;
 import com.atguigu.spzx.manager.service.SysUserService;
 import com.atguigu.spzx.manager.service.ValidateCodeService;
 import com.atguigu.spzx.model.dto.system.LoginDto;
+import com.atguigu.spzx.model.entity.system.SysUser;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
@@ -42,5 +43,13 @@ public class IndexController {
     public Result<ValidateCodeVo> generateValidateCode() {
         ValidateCodeVo validateCodeVo = validateCodeService.generateValidateCode();
         return Result.build(validateCodeVo,ResultCodeEnum.SUCCESS);
+    }
+
+    //获取登录用户信息
+    @GetMapping("getUserInfo")
+    @Operation(summary = "获取用户信息的方法")
+    public Result getUserInfo(@RequestHeader(name = "token") String token){
+        SysUser sysUser= sysUserService.getUserInfo(token);
+        return Result.build(sysUser,ResultCodeEnum.SUCCESS);
     }
 }
